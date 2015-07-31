30cbd4176f1842a392c3e53b80e08027638a2169
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-07-31 15:07:57 +0300

    Life

---
+++ Growth test v.7/Assets/Scripts/Life.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+
+	public GameObject life;
+	GameObject parentHex;
+
+	int radius;
+
+//	void Awake () {
+//		parentHex = gameObject.transform.parent;
+//
+//		int X = parentHex.GetComponent<Resourse> ().xPos;
+//		int Y = parentHex.GetComponent<Resourse> ().yPos;
+//		int R = radius;
+//		for(int y = Mathf.Max (Y - R, 0); y <= Mathf.Min (Y + R, axisGrid.gridHeightInHexes - 1); y ++) {
+//			for(int x = Mathf.Max(X - R, X - R + y - Y, 0 + y / 2); x <= Mathf.Min(X + R, X + R + y - Y, axisGrid.gridWidthInHexes + y / 2 - 1); x ++) {
+//				Hexes[x + y * key].GetComponent<Renderer> ().material = colors [materialInUse];
+//			}
+//		}
+//	}
+
+	void Update () {
+	
+	}
+}
