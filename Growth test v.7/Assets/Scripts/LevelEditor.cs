f65a6d2dadb219a435b5baeaab9d2f6ccfdad731
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-08-28 17:21:11 +0300

    level editor raycast responding

--- Growth test v.7/Assets/Scripts/LevelEditor.cs
+++ Growth test v.7/Assets/Scripts/LevelEditor.cs
@@ -6,6 +6,9 @@ public class LevelEditor : EditorWindow {
 
 	string drawMode = "Draw";
 	string editMode = "Edit";
+	string rayObject;
+	int events = 0;
+	Vector2 mouseLastPos;
 
 	[MenuItem ("Level Editor/Editor Window")]
 	static void Init () {
@@ -21,15 +24,49 @@ public class LevelEditor : EditorWindow {
 		if (GUILayout.Button (drawMode)) {
 		}
 
+		GUILayout.Label (rayObject);
+	}
+
+	void OnEnable () {
+		SceneView.onSceneGUIDelegate += OnSceneGUI;
+	}
+
+	void OnDisable () {
+		SceneView.onSceneGUIDelegate -= OnSceneGUI;
+	}
+
+	public void OnSceneGUI (SceneView sceneview) {
+		mouseLastPos = Event.current.mousePosition;
+
+		if (Camera.current) {
+			Ray camRay = HandleUtility.GUIPointToWorldRay (mouseLastPos);
+			RaycastHit hitPoint;
+			
+			if (Physics.Raycast (camRay, out hitPoint, 100f)) {
+				rayObject = hitPoint.collider.name;
+				Repaint();
+				Resourse r = hitPoint.collider.gameObject.GetComponent<Resourse>();
+				if (r) {
+					r.water = 10;
+				}
+			}
+		}
+		events++; Debug.Log(mouseLastPos);
+		if (Event.current.type == EventType.MouseMove) {
+			Event.current.Use ();
+		}
 	}
 
 	void Update () {
 
-//		Ray camRay = Camera.current.ScreenPointToRay (Input.mousePosition);
-//		RaycastHit hitPoint;
+//		if (Camera.current) {
+//			Ray camRay = HandleUtility.GUIPointToWorldRay (mouseLastPos);
+//			RaycastHit hitPoint;
 //
-//		if (Physics.Raycast (camRay, out hitPoint, 100f)) {
-//			Debug.Log (hitPoint.collider.name);
+//			if (Physics.Raycast (camRay, out hitPoint, 100f)) {
+//				rayObject = hitPoint.collider.name;
+//				Repaint();
+//			}
 //		}
 	}
 }
