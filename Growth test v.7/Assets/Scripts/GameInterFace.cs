45b6d03f3105c5683f73d0d27d1003befa222473
Rx-1 <roni.t.lipsunen@gmail.com>
2015-08-31 14:09:33 +0300

    GameInterface

---
+++ Growth test v.7/Assets/Scripts/GameInterFace.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInterFace : MonoBehaviour {
+
+	public GameObject[] plants;
+
+	bool hasIndex = false;
+	int index = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void Plant1 () {
+		hasIndex = true;
+		index = 0;
+	}
+
+	void Plant2 () {
+		hasIndex = true;
+		index = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasIndex && !Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.Mouse0)) {
+			hasIndex = false;
+			Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitPoint;
+			if (Physics.Raycast (camRay, out hitPoint,  1 << 8) && !hitPoint.collider.transform.FindChild ("Plant")) {
+				GameObject plantIns = (GameObject)Instantiate (plants[index]);
+				plantIns.transform.position = hitPoint.collider.gameObject.transform.position;
+				plantIns.transform.parent = hitPoint.collider.gameObject.transform;
+				plantIns.name = "Plant";
+			}
+		}
+	}
+}
