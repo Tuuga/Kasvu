9d42135349badacbccd1b913f4bf3d90c6e3db2e
Rx-1 <roni.t.lipsunen@gmail.com>
2015-08-31 15:13:04 +0300

    no message

--- Growth test v.7/Assets/Scripts/GameInterFace.cs
+++ Growth test v.7/Assets/Scripts/GameInterFace.cs
@@ -3,38 +3,37 @@ using System.Collections;
 
 public class GameInterFace : MonoBehaviour {
 
-	public GameObject[] plants;
+	public static int[] seeds = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-	bool hasIndex = false;
-	int index = 0;
+	bool hasPlant = false;
+	GameObject plant;
 
 	// Use this for initialization
 	void Start () {
 
 	}
 
-	public void Plant1 () {
-		hasIndex = true;
-		index = 0;
-	}
-
-	void Plant2 () {
-		hasIndex = true;
-		index = 1;
+	public void Plant1 (GameObject buttonPlant) {
+		if (seeds [buttonPlant.GetComponent<Plant> ().seedIndex] > 0) {
+			hasPlant = true;
+			plant = buttonPlant;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (hasIndex && !Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.Mouse0)) {
-			hasIndex = false;
+		if (hasPlant && !Input.GetKey (KeyCode.LeftShift) && Input.GetKey (KeyCode.Mouse0)) {
+			hasPlant = false;
 			Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hitPoint;
 			if (Physics.Raycast (camRay, out hitPoint,  1 << 8) && !hitPoint.collider.transform.FindChild ("Plant")) {
-				GameObject plantIns = (GameObject)Instantiate (plants[index]);
+				seeds[plant.GetComponent<Plant>().seedIndex] -= 1;
+				GameObject plantIns = (GameObject)Instantiate (plant);
 				plantIns.transform.position = hitPoint.collider.gameObject.transform.position;
 				plantIns.transform.parent = hitPoint.collider.gameObject.transform;
 				plantIns.name = "Plant";
 			}
+			plant = null;
 		}
 	}
 }
