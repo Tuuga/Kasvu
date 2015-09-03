37167e1ee912a53064d9add6db7e1a9d39232e87
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-09-03 14:28:37 +0300

    level select

---
+++ Growth test v.7/Assets/LevelManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagerScript : MonoBehaviour {
+
+	public Transform[] mark;
+	int markInt;
+	public GameObject cameraObj;
+	public float speed;
+	bool moveCameraRight;
+	bool moveCameraLeft;
+
+	void Update () {
+
+		if (moveCameraRight) {
+			cameraObj.transform.localPosition += new Vector3 (1, 0, 0) * Time.deltaTime * speed;
+		}
+		if (Vector3.Distance (cameraObj.transform.position, mark[markInt].position) < 0.5f) {
+			moveCameraRight = false;
+		}
+		if (moveCameraLeft) {
+			cameraObj.transform.localPosition += new Vector3 (-1, 0, 0) * Time.deltaTime * speed;
+		}
+		if (Vector3.Distance (cameraObj.transform.position, mark[markInt].position) < 0.5f) {
+			moveCameraLeft = false;
+		}
+	}
+
+	public void LeftArrow () {
+		markInt = Mathf.Clamp (markInt - 1, 0, 5);
+		if (markInt < 5 && markInt > 0) {
+			moveCameraLeft = true;
+		}
+	}
+
+	public void RightArrow () {
+		if (markInt < 5 && markInt >= 0) {
+			Debug.Log (markInt);
+			markInt = Mathf.Clamp (markInt + 1, 0, 5);
+			Debug.Log (markInt);
+			moveCameraRight = true;
+		}
+	}
+}
